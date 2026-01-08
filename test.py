9d9227499ea0aba9c789c13af3a21ff67a8b886b
halusee <heesung.lee33@gmail.com>
2026-01-08 07:01:29 -0800

    add sim.py

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-import numpy as py
-print(hello world);
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+
+ax = plt.axes(projection="3d")
+
+x = np.arange(-5, 5, 0.1)
+y = np.arange(-5, 5, 0.1)
+
+X, Y = np.meshgrid(x, y)
+Z = np.sin(X) * np.cos(Y)
+
+ax.plot_surface(X, Y, Z)
+ax.set_title("3d plot")
+ax.set_xlabel("test")
+plt.show()
